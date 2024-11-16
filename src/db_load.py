e6f5a9e29617ef95021027c9f9609b7e32efcd1c
Lisa <lisahafizovic@gmail.com>
2024-11-16 14:02:14 +0100

    feat: persist family query to DB table

--- src/db_load.py
+++ src/db_load.py
@@ -7,9 +7,20 @@ def persist_db(src_file, db_file):
   # Increase sample size to avoid DuckDB guessing the wrong type because of null values.
   con.execute("SET GLOBAL pandas_analyze_sample=10000")
   df = pd.read_csv(src_file)
+  sql_query = open('./src/sql/family.sql', 'r').read()
 
+  # Recreate tables.
+  con.execute("DROP TABLE IF EXISTS ancestry")
+  con.execute("DROP TABLE IF EXISTS family")
+
+  # Insert new data into ancestry src table.
   con.execute("CREATE TABLE ancestry AS SELECT * FROM df")
-  con.execute("INSERT INTO ancestry SELECT * FROM df")
+
+  # Query to only select direct ancestors and exclude any non-related spouses and half-siblings of ancestors.
+  df_fam = pd.read_sql(sql_query, con)
+
+  # Create new family table from family query.
+  con.execute("CREATE TABLE family AS SELECT * FROM df_fam")
 
   con.close()
 
@@ -20,4 +31,4 @@ if __name__ == '__main__':
 
   persist_db(input_file, db_file_name)
 
-  print('DONE.')
+  print(f"Written {input_file} to {db_file_name}.")
